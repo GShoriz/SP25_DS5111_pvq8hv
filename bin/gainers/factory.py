7ffc24c00a8f493c3ca80d061fbe2b261b0e913d
GShoriz <pvq8hv@virginia.edu>
2025-03-26 01:35:56 +0000

    updating syntax errors with pylint suggestions

--- bin/gainers/factory.py
+++ bin/gainers/factory.py
@@ -1,25 +1,38 @@
+"""
+Module provides a factory class to create downloader and processor objects based on a given source.
+"""
+
 from base import GainerDownload, GainerProcess
 from yahoo import GainerDownloadYahoo, GainerProcessYahoo
 from wsj import GainerDownloadWSJ, GainerProcessWSJ
 from cnbc import GainerDownloadCNBC, GainerProcessCNBC
 
 class GainerFactory:
+	"""
+	Factory class to instantiate downloader and processor objects for various sources.
+	"""
 	def __init__(self, choice):
 		assert choice in ['yahoo', 'wsj', 'cnbc', 'test'], f"Unrecognized gainer type {choice}"
 		self.choice = choice
 
 	def get_downloader(self):
+		"""
+		Returns an instance of the appropriate downloader based on the chosen source.
+		"""
 		if self.choice == 'yahoo':
 			return GainerDownloadYahoo()
-		elif self.choice == 'wsj':
+		if self.choice == 'wsj':
 			return GainerDownloadWSJ()
-		elif self.choice == 'cnbc':
+		if self.choice == 'cnbc':
 			return GainerDownloadCNBC()
 
 	def get_processor(self):
+		"""
+		Returns an instance of the appropriate processor based on the chosen source.
+		"""
 		if self.choice == 'yahoo':
 			return GainerProcessYahoo()
-		elif self.choice == 'wsj':
+		if self.choice == 'wsj':
 			return GainerProcessWSJ()
-		elif self.choice == 'cnbc':
+		if self.choice == 'cnbc':
 			return GainerProcessCNBC()
