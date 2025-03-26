dcddeaf3a0850ca55d8296b175f0d8a9af74fb27
GShoriz <pvq8hv@virginia.edu>
2025-03-26 04:19:36 +0000

    made changes with pylint suggestions

--- bin/gainers/factory.py
+++ bin/gainers/factory.py
@@ -1,38 +1,41 @@
 """
-Module provides a factory class to create downloader and processor objects based on a given source.
+Factory Module to direct calls to specific gainers.
 """
-
-from base import GainerDownload, GainerProcess
 from yahoo import GainerDownloadYahoo, GainerProcessYahoo
 from wsj import GainerDownloadWSJ, GainerProcessWSJ
 from cnbc import GainerDownloadCNBC, GainerProcessCNBC
 
 class GainerFactory:
-	"""
-	Factory class to instantiate downloader and processor objects for various sources.
-	"""
-	def __init__(self, choice):
-		assert choice in ['yahoo', 'wsj', 'cnbc', 'test'], f"Unrecognized gainer type {choice}"
-		self.choice = choice
+    """
+    Factory class to instantiate downloader and processor objects for various sources.
+    """
+    def __init__(self, choice):
+        """
+        Initializes the factory with the specified choice of gainer type.
+        """
+        assert choice in ['yahoo', 'wsj', 'cnbc', 'test'], f"Unrecognized gainer type {choice}"
+        self.choice = choice
 
-	def get_downloader(self):
-		"""
-		Returns an instance of the appropriate downloader based on the chosen source.
-		"""
-		if self.choice == 'yahoo':
-			return GainerDownloadYahoo()
-		if self.choice == 'wsj':
-			return GainerDownloadWSJ()
-		if self.choice == 'cnbc':
-			return GainerDownloadCNBC()
+    def get_downloader(self):
+        """
+        Returns an instance of the appropriate downloader based on the chosen source.
+        """
+        if self.choice == 'yahoo':
+            return GainerDownloadYahoo()
+        elif self.choice == 'wsj':
+            return GainerDownloadWSJ()
+        elif self.choice == 'cnbc':
+            return GainerDownloadCNBC()
+        raise ValueError(f"Unrecognized gainer type: {self.choice}")
 
-	def get_processor(self):
-		"""
-		Returns an instance of the appropriate processor based on the chosen source.
-		"""
-		if self.choice == 'yahoo':
-			return GainerProcessYahoo()
-		if self.choice == 'wsj':
-			return GainerProcessWSJ()
-		if self.choice == 'cnbc':
-			return GainerProcessCNBC()
+    def get_processor(self):
+        """
+        Returns an instance of the appropriate processor based on the chosen source.
+        """
+        if self.choice == 'yahoo':
+            return GainerProcessYahoo()
+        elif self.choice == 'wsj':
+            return GainerProcessWSJ()
+        elif self.choice == 'cnbc':
+            return GainerProcessCNBC()
+        raise ValueError(f"Unrecognized gainer type: {self.choice}")
